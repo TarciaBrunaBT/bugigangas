2d621683fa9f9f2b88198001f7342e43e5fe22d2
Burna <tbbt.91@gmail.com>
2017-09-30 21:37:37 -0400

    arquivo incial do projeto

---
+++ bugigangas-api/src/main/java/br/com/bugigangas/bugigangas/BugigangasApplication.java
@@ -0,0 +1,12 @@
+package br.com.bugigangas.bugigangas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugigangasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugigangasApplication.class, args);
+	}
+}
