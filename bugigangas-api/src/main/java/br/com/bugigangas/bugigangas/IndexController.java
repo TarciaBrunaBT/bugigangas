e8479ecbb325d039384325059359fd6602e91347
joelsnob <joeloliaragao@gmail.com>
2017-10-01 13:30:25 -0400

    configuração do banco inicicial

--- bugigangas-api/src/main/java/br/com/bugigangas/bugigangas/IndexController.java
+++ bugigangas-api/src/main/java/br/com/bugigangas/bugigangas/IndexController.java
@@ -1,12 +1,15 @@
 package br.com.bugigangas.bugigangas;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
-	@RequestMapping("/")
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
 		return "index";
 	}
+	
 }
